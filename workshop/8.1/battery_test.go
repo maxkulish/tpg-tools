2512545b12824791ef930371a83d5a328c13e2e1
Max Kulish <kma.memo@gmail.com>
2022-05-21 16:38:40 +0300

    feat: chapter 8

---
+++ workshop/8.1/battery_test.go
@@ -0,0 +1,32 @@
+//go:build integration
+
+package battery_test
+
+import (
+	"battery"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestParsePmsetOutput(t *testing.T) {
+	t.Parallel()
+
+	data, err := os.ReadFile("testdata/pmset.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := battery.Status{
+		ChargePercent: 98,
+	}
+	got, err := battery.ParsePmsetOutput(string(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
